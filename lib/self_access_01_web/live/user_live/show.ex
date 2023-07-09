0513abd9540fe86d76cee137d01ae9143e503787
FRICK-ELDY <ncssano0x0000@gamil.com>
2023-07-09 12:33:00 +0900

    run mix.gen.live and fix router.ex

---
+++ lib/self_access_01_web/live/user_live/show.ex
@@ -0,0 +1,21 @@
+defmodule SelfAccess01Web.UserLive.Show do
+  use SelfAccess01Web, :live_view
+
+  alias SelfAccess01.Accounts
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(%{"id" => id}, _, socket) do
+    {:noreply,
+     socket
+     |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:user, Accounts.get_user!(id))}
+  end
+
+  defp page_title(:show), do: "Show User"
+  defp page_title(:edit), do: "Edit User"
+end
